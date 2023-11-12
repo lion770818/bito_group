c442a9edd67bb46abf8188e765c3ac785d5fce82
close-leo <close0818@gmail.com>
2023-11-12 09:20:09 +0800

    add: swagger doc

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# bito_group
-作業
+go get -u github.com/swaggo/swag/cmd/swag
+go install github.com/swaggo/swag/cmd/swag@latest
+
+swag init
+
+go get -u github.com/swaggo/gin-swagger
+go get -u github.com/swaggo/files
--- main.go
+++ main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"bito_group/docs"
 	"encoding/json"
 	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	//docs "github.com/go-project-name/docs"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
+
+// PingExample godoc
+// @Summary ping example
+// @Schemes
+// @Description do ping
+// @Tags example
+// @Accept json
+// @Produce json
+// @Success 200 {string} Helloworld
+// @Router /example/helloworld [get]
+func Helloworld(g *gin.Context) {
+	g.JSON(http.StatusOK, "helloworld")
+}
 
 func AddSinglePersonAndMatch(c *gin.Context) {
 
@@ -37,9 +54,17 @@ func main() {
 
 	router := gin.New()
 
-	router.POST("/AddSinglePersonAndMatch", AddSinglePersonAndMatch)
-	router.DELETE("/RemoveSinglePerson", RemoveSinglePerson)
-	router.POST("/QuerySinglePeople", QuerySinglePeople)
+	docs.SwaggerInfo.BasePath = "/api/v1"
+	v1 := router.Group("/api/v1")
+	{
 
+		v1.POST("/AddSinglePersonAndMatch", AddSinglePersonAndMatch)
+		v1.DELETE("/RemoveSinglePerson", RemoveSinglePerson)
+		v1.POST("/QuerySinglePeople", QuerySinglePeople)
+
+		v1.GET("/helloworld", Helloworld)
+	}
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 	router.Run("0.0.0.0:8080")
 }
